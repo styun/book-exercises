48e66d0a0c6c3554792dd72cc2bb7646ff4d99e0
samuelyun <styun99@gmail.com>
2020-01-24 17:25:43 -0800

    Week 3 Chapter 10

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -3,20 +3,20 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
-
+employees <- c(paste("Employee", 1))
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
+salaries <- runif(100, 40000, 50000)
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
-
+salary <- runif(100, -5000, 10000)
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, salaries, salary, stringsAsFactors = FALSE)
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
